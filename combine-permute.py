447d05c271efd2eb67b6af7fb53c95b4550635ba
Deathsteps <lovely_dreamer@126.com>
2022-07-08 13:18:30 +0800

    Create combine-permute.py

---
+++ combine-permute.py
@@ -0,0 +1,16 @@
+# 组合与排列
+
+# https://leetcode-cn.com/problems/combinations/
+
+# FIXME:
+# 递归展开 https://zhuanlan.zhihu.com/p/136511316
+# https://www.zhihu.com/question/20418254
+# 尾递归 https://www.ruanyifeng.com/blog/2015/04/tail-call.html
+
+
+# =======
+
+# https://leetcode-cn.com/problems/letter-case-permutation/
+
+# FIXME: 这题可以用，笛卡尔乘积（数学）或 二分掩码 求解
+# https://segmentfault.com/a/1190000039239875
