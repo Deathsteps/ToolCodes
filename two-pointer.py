a4000a6e49f334d4c248820f4302fd72690f1669
Deathsteps <lovely_dreamer@126.com>
2022-07-08 11:13:35 +0800

    Create two-pointer.py

---
+++ two-pointer.py
@@ -0,0 +1,14 @@
+# https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted/
+# 双指针的本质更像是编程技巧
+# 多用于有序数组或字符串类题目
+
+# https://leetcode-cn.com/problems/squares-of-a-sorted-array/
+
+# https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted/
+
+# https://leetcode-cn.com/problems/he-wei-sde-liang-ge-shu-zi-lcof/
+
+# https://leetcode-cn.com/problems/rotate-array/
+# 实现生活中的内存或着容量限制很常见
+# 这个题我理解下来作用在于
+# 锻炼大脑，在有限容量下，探索一个可行方法（常常是暴力算法），再由可行方法探索一个快速方法
